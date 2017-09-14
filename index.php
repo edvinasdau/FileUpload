b8f34ba0e80658fdc070b7073ed6a080b0b05683
edvinasdau <edvinasdau@gmail.com>
2017-09-14 13:18:42 +0300

    uploaded

--- index.php
+++ index.php
@@ -1,19 +1,32 @@
 <?php
 
-$max_size = 1024;
-$allowed_ext = array('jpg', 'png', 'gif');
-
 if (isset($_FILES['image'])){
-	if ($_FILES['image']['size'] < $max_size) {
+		$file = $_FILES['image'];
 
-		$file_name_array = explode(".",$_FILES['image']['name']);
-		$file_extension = $file_name_array[1];
+		$file_name = $file['name'];
+		$file_tmp = $file['tmp_name'];
+		$file_size = $file['size'];
+		$file_error = $file['error'];
 
-		if (in_array($file_extension, $allowed_ext)) {
-			
-			move_uploaded_file($_FILES['image'], ['tmp_name'])
-		}
-	}
+		$file_ext = explode('.', $file_name);
+		$file_ext = strtolower(end($file_ext));
+
+		$allowed = array('jpg', 'gif');
+
+		if(in_array($file_ext, $allowed)){
+			if($file_error === 0){
+				if($file_size <= 1048576){
+
+					$file_name_new = uniqid('', true) . '.' . $file_ext;
+					$file_destination = 'uploads/' . $file_name_new;
+
+					if(move_uploaded_file($file_tmp, $file_destination)){
+						echo $file_destination;
+					}
+
+				}
+			}
+		}	
 }
 
 ?>
